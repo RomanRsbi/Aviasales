10213d54c2e37c8855c01f2994a8f75b7f82a1ed
Roman <romanrsbi@gmail.com>
2025-01-04 18:43:13 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <main>
+        <img src="/logo/Logo.svg" alt="logo" />
+        <section>
+          <aside></aside>
+          <div>
+            <ul>
+              <li>
+                <button>Самый дешевый</button>
+              </li>
+              <li>
+                <button>Самый быстрый</button>
+              </li>
+              <li>
+                <button>Оптимальный</button>
+              </li>
+            </ul>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export { App };
